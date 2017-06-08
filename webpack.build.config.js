d3f7b734551b526b09ac5c10d98ce8cc69d7ede3
fei xu <1065543971@QQ.COM>
2017-06-08 17:17:45 +0800

    add git ignore and webpack config

---
+++ webpack.build.config.js
@@ -0,0 +1,37 @@
+var webpack = require('webpack');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    bundle: './src/app.es6',
+  },
+  output: {
+    path: __dirname + '/build',
+    filename: '[name].js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.es6$/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({ fallback: "style-loader", use: "css-loader" })
+      },
+      {
+        test: /\.(png|jpg|gif|ico)$/,
+        loader: 'url-loader?limit=8096&name=images/[hash].[ext]'
+　　　　}
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'html-withimg-loader!' + __dirname + "/src/index.html"
+    }),
+    new ExtractTextPlugin("style.css"),
+    new webpack.optimize.UglifyJsPlugin(),
+  ]
+};
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    bundle: './src/app.es6',
+  },
+  output: {
+    path: __dirname + '/public',
+    filename: '[name].js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(es6|js)$/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({ fallback: "style-loader", use: "css-loader" })
+      },
+      {
+        test: /\.(png|jpg|gif|ico)$/,
+        loader: 'url-loader?limit=8192&name=images/[name].[ext]'
+　　　　}
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'html-withimg-loader!' + __dirname + "/src/index.html"
+    }),
+    new ExtractTextPlugin("style.css")
+  ]
+};
